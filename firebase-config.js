3f9a4dab94d93bac73479b7c463b08de164087a1
Cuong <anhcuongnhacai@gmail.com>
2026-05-15 15:14:00 +0700

    update

--- firebase-config.js
+++ firebase-config.js
@@ -1,33 +1,18 @@
 // firebase-config.js
-// --------------------------
-// Import các hàm cần thiết từ Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getDatabase, ref, push, onValue, set, get } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-database.js";
 
-// ===========================
-// Cấu hình Firebase (thay bằng config của bạn)
-// ===========================
 const firebaseConfig = {
-  apiKey: "AIzaSyARRhfDvEterUOjMnCdjMuV0pRRt4OFrYI",
-  authDomain: "goldwt1-f597e.firebaseapp.com",
-  databaseURL: "https://goldwt1-f597e-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "goldwt1-f597e",
-  storageBucket: "goldwt1-f597e.firebasestorage.app",
-  messagingSenderId: "301134547289",
-  appId: "1:301134547289:web:59c2bead386d343bb2e448"
+  apiKey: "AIzaSyAh0hpuNtXrqJZbFLH7Bs6A7vUo73sEqIU",
+  authDomain: "goldwt123.firebaseapp.com",
+  databaseURL: "https://goldwt123-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "goldwt123",
+  storageBucket: "goldwt123.appspot.com",
+  messagingSenderId: "340852437228",
+  appId: "1:340852437228:web:e64444db949896d69a35c9"
 };
 
-// ===========================
-// Khởi tạo Firebase app
-// ===========================
 const app = initializeApp(firebaseConfig);
-
-// ===========================
-// Khởi tạo Database
-// ===========================
 const db = getDatabase(app);
 
-// ===========================
-// Export để các file khác import
-// ===========================
 export { db, ref, push, onValue, set, get };
